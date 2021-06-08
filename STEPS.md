44a7bac3fc994624d1cf6aba7f2ad91372e69da1
Reinoptland <reinoptland@gmail.com>
2021-06-08 16:45:56 +0200

    Trigger a rerender

--- STEPS.md
+++ STEPS.md
@@ -16,8 +16,8 @@ Fetching
 
 Trigger a rerender
 
-- [ ] make a state to store the data
-- [ ] when you get a response in useEffect, setState (store the data)
+- [x] make a state to store the data
+- [x] when you get a response in useEffect, setState (store the data)
 
 Present the data in the new update cycle
 
--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [movies, setMovies] = useState(null);
+  console.log("WHAT IS THE STATE:", movies);
+
   useEffect(() => {
     console.log("ON MOUNT:");
     async function fetchMovies() {
@@ -10,10 +13,17 @@ function App() {
         "http://www.omdbapi.com/?s=love&apikey=804956f8"
       );
       console.log(response.data.Search);
+      setMovies(response.data.Search);
     }
 
     fetchMovies();
   }, []);
+
+  // 1. "WHAT IS THE STATE:" null
+  // 2. "ON MOUNT:"
+  // ... even wachtten
+  // 3. console.log([{}, {}, {}]); -> de data
+  // 4. console.log("WHAT IS THE STATE:", [{}, {}, {}]);
 
   return (
     <div className="App">
